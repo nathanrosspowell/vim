1ef056a50b77fb52a5cc2a0acc4ad4f325335365
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-31 00:50:39 -0400

    Work From The Current Scripts Dir

--- install.bash
+++ install.bash
@@ -1,33 +1,39 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Function to remove files and create symbolic links.
+create_new_symlink () {
+    if [ -f $2 ];
+    then
+        echo Removing file $2
+        rm $2
+    elif [ -h $2 ];
+    then
+        echo Removing symbolic link to $2
+        rm $2
+    fi
+    echo New  symbolic link $1 to $2
+    ln -s --relative $1 $2
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Install all needed software.
-packageList=(
+package_list=(
     curl
     vim
     ttf-inconsolata
     exuberant-ctags
 )
-sudo apt-get install ${packagelist[@]}
+sudo apt-get install ${package_list[@]}
 sudo apt-get autoremove
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get the current dir of this script.
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd current_dir
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Symbolic links for vim
-currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ -f ~/.vim ];
-then
-    rm ~/.vim
-fi
-ln -s $currentDir ~/.vim
+create_new_symlink . ~/.vim
 # Symbolic links for vimrc
-if [ -f ~/.vimrc ];
-then
-    rm ~/.vimrc
-fi
-ln -s $currentDir/vimrc ~/.vimrc
+create_new_symlink vimrc ~/.vimrc
 # Symbolic links for gvimrc
-if [ -f ~/.gvimrc ];
-then
-    rm ~/.gvimrc
-fi
-ln -s $currentDir/gvimrc ~/.gvimrc
+create_new_symlink gvimrc ~/.gvimrc
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Make sure there is a plugin directory.
 if [ ! -d bundle ]
@@ -36,8 +42,7 @@ then
 fi
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # git submodules.
-git submodule init
-git submodule update
+git submodule update --init --recursive
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Launch vim. This will output errors if things went wrong.
-vim README.md
+popd
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
