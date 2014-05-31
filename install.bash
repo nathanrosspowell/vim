8254c0ad758eb552bc5679f7ec672ed165f7c7e3
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-05-30 21:33:33 -0400

    made the install path work for all folder names

--- install.bash
+++ install.bash
@@ -10,23 +10,24 @@ sudo apt-get install ${packagelist[@]}
 sudo apt-get autoremove
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Symbolic links for vim
+currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -f ~/.vim ];
 then
     rm ~/.vim
 fi
-ln -s ~/vim/ ~/.vim
+ln -s $currentDir ~/.vim
 # Symbolic links for vimrc
 if [ -f ~/.vimrc ];
 then
     rm ~/.vimrc
 fi
-ln -s ~/vim/vimrc ~/.vimrc
+ln -s $currentDir/vimrc ~/.vimrc
 # Symbolic links for gvimrc
 if [ -f ~/.gvimrc ];
 then
     rm ~/.gvimrc
 fi
-ln -s ~/vim/gvimrc ~/.gvimrc
+ln -s $currentDir/gvimrc ~/.gvimrc
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Make sure there is a plugin directory.
 if [ ! -d bundle ]
